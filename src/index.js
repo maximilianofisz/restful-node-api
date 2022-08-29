9753161d3dda6dbeb7c4466e742a96c255c6f86d
mfisz <maxifisz@gmail.com>
2022-08-29 13:50:21 -0300

    working basic crud api with swagger

--- src/index.js
+++ src/index.js
@@ -2,15 +2,38 @@ let express = require('express')
 let app = express()
 let storeRoute = require('./routes/store')
 let bodyParser = require('body-parser')
+let swaggerUI = require('swagger-ui-express')
+let swaggerJSDocument = require('swagger-jsdoc')
 
+//Swagger
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: "My stores API",
+            description: "A descriptive description!",
+            contact: {
+                name: "me"
+            },
+            servers: ["http://localhost:3000"]
+        }
+    },
+    apis: ["src/routes/*.js"]
+}
 
+const swaggerDocs = swaggerJSDocument(swaggerOptions)
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
+
+//----------------------------------------------------------------------
+
+// Main middlewares
 app.use(bodyParser.json())
 app.use( (req, res, next) => {
     console.log(`${new Date().toString()} => ${req.originalUrl}`)
     next()
 })
 app.use(storeRoute)
-app.use(express.static('public'))
 app.use( (req, res, next) =>{
     res.status(404).send('We think you are lost!')
 })
@@ -19,7 +42,7 @@ app.use( (err, req, res, next) =>{
     res.status(500).send('There was an internal server error!')
 })
 
-
+//---------------------------------------------------------------------
 
 
 const PORT = process.env.PORT || 3000
--- src/routes/store.js
+++ src/routes/store.js
@@ -2,6 +2,51 @@ let express = require('express')
 let router = express.Router()
 let storeModel = require('../models/store')
 
+/**
+* @swagger
+* components:
+*   schemas:
+*     Store: 
+*       type: object
+*       required:
+*         - name
+*         - adress
+*         - category
+*         - rating
+*       properties:
+*         name:
+*           type: string
+*         adress:
+*           type: string
+*         category:
+*           type: string
+*         rating:
+*           type: number
+*       example:
+*            name: Compra Gamer
+*            adress: Gamer Avenue 420
+*            category: Electronics
+*            rating: 5
+*/
+
+
+/**
+*  @swagger
+*  /store:
+*   post:
+*     summary: Create a new store
+*     tags: [Stores]
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             type: object
+*             $ref: '#/components/schemas/Store'
+*     responses:  
+*       200:
+*         description: New store created!
+*/
 router.post('/store', (req, res)=>{
      if(!req.body){
         return res.status(400).send('Request body is missing')
@@ -19,6 +64,22 @@ router.post('/store', (req, res)=>{
      })     
 })
 
+/**
+*  @swagger
+*  /stores:
+*   get:
+*     summary: Gets all stores
+*     tags: [Stores]
+*     responses:  
+*       200:
+*         description: All stores!
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 $ref: '#/components/schemas/Store'
+*/
 router.get('/stores', (req, res) =>{
     storeModel.find()
     .then(doc => {
@@ -29,9 +90,33 @@ router.get('/stores', (req, res) =>{
     })
 })
 
-router.delete('/stores', (req, res) =>{
-    storeModel.findOneAndRemove({
-        name: req.query.name
+
+
+/**
+*  @swagger
+*  /store/{name}:
+*   delete:
+*     summary: deletes a store
+*     tags: [Stores]
+*     parameters:
+*       - in: path
+*         name: name
+*         schema:
+*           type: string
+*         required: true
+*         description: the store name
+*     responses:
+*       200:
+*         description: store deleted
+*       400:
+*         description: store not found
+*
+ */
+router.delete('/store/:name', (req, res) =>{
+    const name  = req.params.name
+    storeModel
+    .deleteMany({
+        name: name
     })
     .then(doc => {
         res.json(doc)
@@ -41,14 +126,49 @@ router.delete('/stores', (req, res) =>{
     })
 })
 
-//TODO
-// router.put()
-
-
-
-
-
-
-
+/**
+*  @swagger
+*   /store/{name}:
+*   put:
+*     summary: update a store
+*     tags: [Stores]
+*     parameters:
+*       - in: path
+*         name: name
+*         schema:
+*           type: string
+*         required: true
+*         description: the store name
+*     requestBody:
+*         required: true
+*         content:
+*           application/json:
+*             schema:
+*               type: object
+*               $ref: '#/components/schemas/Store'
+*     responses:
+*       200:
+*         description: store modified correctly
+*       400:
+*         description: store was not found
+ */
+router.put('/store/:name', (req, res) =>{
+    const name = req.params.name
+    const { adress, rating, category } = req.body
+    storeModel
+    .updateOne(
+        {name: name},
+        {
+            adress: adress,
+            rating: rating,
+            adress: adress
+        })
+    .then(doc => {
+        res.json(doc)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+})
 
 module.exports = router
